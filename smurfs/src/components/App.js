e45105e06e6cf80f988afbc64b062a123ea527a8
vishalicious213 <vishalicious213@gmail.com>
2020-05-01 21:19:53 -0400

    initial commit. added axios. getting data from localhost.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from 'axios';
+
 class App extends Component {
+  getData = () => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(results => {
+        console.log('Results: ', results.data)
+      })
+      .catch(error => console.log('Error: ', error));
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,3 +29,18 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>SMURFS! 2.0 W/ Redux</h1>
+//         <div>Welcome to your state management version of Smurfs!</div>
+//         <div>Start inside of your `src/index.js` file!</div>
+//         <div>Have fun!</div>
+//       </div>
+//     );
+//   }
+// }
