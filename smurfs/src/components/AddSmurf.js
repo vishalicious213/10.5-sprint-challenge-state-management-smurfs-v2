8f6217e201535bbb06c86769867f334e7f2f1df8
vishalicious213 <vishalicious213@gmail.com>
2020-05-03 06:18:21 -0400

    setting up event handlers for AddSmurf name, age & height

--- smurfs/src/components/AddSmurf.js
+++ smurfs/src/components/AddSmurf.js
@@ -4,12 +4,24 @@ const AddSmurf = () => {
     const [name, setName] = useState('');
     const [age, setAge] = useState('');
     const [height, setHeight] = useState('');
-    
+
+    const updateName = event => {
+        setName(event.target.value);
+    }
+
+    const updateAge = event => {
+        setAge(event.target.value);
+    }
+
+    const updateHeight = event => {
+        setHeight(event.target.value);
+    }
+
     return (
         <form>
-            <input type='text' name='name'/>
-            <input type='text' name='age'/>
-            <input type='text' name='height'/>
+            <input type='text' name='name' value={name} onChange={updateName}/>
+            <input type='text' name='age' value={age} onChange={updateAge}/>
+            <input type='text' name='height' value={height} onChange={updateHeight}/>
             <button>Add Smurf</button>
         </form>
     )
