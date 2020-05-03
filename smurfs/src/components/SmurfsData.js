deb6a0082e8d1feea0b15696efbbc83753ab08bd
vishalicious213 <vishalicious213@gmail.com>
2020-05-03 05:57:58 -0400

    cleaned up SmurfsData now that SmurfContext works

--- smurfs/src/components/SmurfsData.js
+++ smurfs/src/components/SmurfsData.js
@@ -1,27 +1,8 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
 import React, { useContext } from 'react';
 import Smurf from './Smurf';
 import { SmurfContext } from './SmurfContext';
 
 const SmurfsData = () => {
-    // const [smurfs, setSmurfs] = useState([]);
-    // const getData = () => {
-    //     axios
-    //         .get('http://localhost:3333/smurfs')
-    //         .then(results => {
-    //             // console.log('Results: ', results.data);
-    //             setSmurfs(results.data);
-    //         })
-    //         .catch(error => console.log('Error: ', error));
-    // }
-
-    // useEffect(() => {
-    //     getData();
-    // }, []);
-
-    // // console.log('Smurfs: ', smurfs)
-
     const [smurfs, setSmurfs] = useContext(SmurfContext);
 
     return (
