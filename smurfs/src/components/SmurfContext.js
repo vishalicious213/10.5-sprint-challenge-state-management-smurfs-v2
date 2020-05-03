d3ef4c42690dbdebc6b54260c0627b2c61375b0c
vishalicious213 <vishalicious213@gmail.com>
2020-05-03 05:36:53 -0400

    exporting SmurfContext and SmurfProvider. added props.children to share state to all wrapped components. wrapped all components in App with SmurfProvider

--- smurfs/src/components/SmurfContext.js
+++ smurfs/src/components/SmurfContext.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
+import axios from 'axios';
 
-export const SmurfProvider = () => {
+// context (shares provider data to components)
+export const SmurfContext = createContext();
+
+// provider (holds the data for context to share)
+export const SmurfProvider = (props) => {
     const [smurfs, setSmurfs] = useState([]);
     const getData = () => {
         axios
@@ -12,6 +17,7 @@ export const SmurfProvider = () => {
             .catch(error => console.log('Error: ', error));
     }
 
+    // initial axios call. runs once (dependency array)
     useEffect(() => {
         getData();
     }, []);
@@ -19,6 +25,8 @@ export const SmurfProvider = () => {
     // console.log('Smurfs: ', smurfs)
 
     return (
-
+        <SmurfContext.Provider>
+            {props.children}
+        </SmurfContext.Provider>
     )
 }
